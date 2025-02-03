068b09fcd696870069b337ad5377103ff92f936e
MrNavaStar <mr.navastar@gmail.com>
2025-02-03 03:19:20 -0700

    verify apple jwt's on the backend

--- backend/src/main/java/com/lyrne/backend/Database.java
+++ backend/src/main/java/com/lyrne/backend/Database.java
@@ -1,15 +1,14 @@
 package com.lyrne.backend;
 
 import java.util.ArrayList;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Database {
 
-    private static final ConcurrentHashMap<UUID, User> fakeDatabase = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, User> fakeDatabase = new ConcurrentHashMap<>();
 
-    public static User getUser(UUID uuid) {
-        return fakeDatabase.computeIfAbsent(uuid, User::new);
+    public static User getUser(String id) {
+        return fakeDatabase.computeIfAbsent(id, User::new);
     }
 
     public static void saveUser(User user) {
--- backend/src/main/java/com/lyrne/backend/Main.java
+++ backend/src/main/java/com/lyrne/backend/Main.java
@@ -1,36 +1,25 @@
 package com.lyrne.backend;
 
+import com.google.gson.Gson;
 import io.javalin.Javalin;
-import io.javalin.http.Context;
-import io.javalin.http.UnauthorizedResponse;
-import io.javalin.security.RouteRole;
 
 public class Main {
 
-    private static void handleLogin(Context ctx) {
-
-    }
+    public static final Gson GSON = new Gson();
 
     public static void main(String[] args) {
         Javalin.create(config -> {
             config.bundledPlugins.enableRouteOverview("/");
             config.staticFiles.add("web");
         })
+                .before("/api/private/*", AuthManager::authenticate)
 
-                .before("/api/private/*", ctx -> {
-                    // Verify user is authenticated here
-                    // Set user roles with ctx.getRoles().add();
-                    throw new UnauthorizedResponse();
-                })
                 // This handler has no auth, anyone can ping it
                 .get("/api/some/fancy/endpoint", ctx -> ctx.result("Hello World"))
                 // This handler has auth, must be logged in
                 .get("/api/private/some/fancy/endpoint", ctx -> ctx.result("Hello World"), User.Role.ANYONE)
                 // This handler has auth, and the user must be admin
                 .get("/api/private/some/other/endpoint", ctx -> ctx.result("Hello World"), User.Role.ADMIN)
-
-
-                .get("/api/login", Main::handleLogin, User.Role.ANYONE)
 
                 .start(8820);
     }
