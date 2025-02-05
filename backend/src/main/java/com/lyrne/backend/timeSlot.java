66f6e75e78d51eb326486440628e9e9e6e9ebecc
ElijahG-ME <greige@mymacewan.ca>
2025-02-04 21:54:05 -0700

    filled out some of the timeslot object with actual code. also probably doesnt work

--- backend/src/main/java/com/lyrne/backend/timeSlot.java
+++ backend/src/main/java/com/lyrne/backend/timeSlot.java
@@ -14,7 +14,7 @@ public class timeSlot{ // A timeslot object that can be created by a tutor
     public void timeSlot(DateTime start, DateTime end, String tutorName, String[] subjects){ 
         // note: not sure what type of object the start/end time will be. its a DateTime here but i'll change it accordingly
         
-        // this looks ridiculous so im sure there will be a better way to get a time object from the front end
+ 
 
         this.timeSlotInterval = new Interval(start, end); // i think this is how you do it in java tho ngl im copying an example
         this.subjects = subjects;
