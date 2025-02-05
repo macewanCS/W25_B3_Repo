5d4cf3efd681c1005fc52d2eafa7b35bac8300c5
MrNavaStar <mr.navastar@gmail.com>
2025-02-04 21:02:15 -0700

    add sqlib to project

--- backend/src/main/java/com/lyrne/backend/Main.java
+++ backend/src/main/java/com/lyrne/backend/Main.java
@@ -1,13 +1,20 @@
 package com.lyrne.backend;
 
 import com.google.gson.Gson;
+import com.lyrne.backend.services.AuthManager;
 import io.javalin.Javalin;
+import me.mrnavastar.sqlib.impl.config.SQLibConfig;
+
+import java.nio.file.Path;
 
 public class Main {
 
     public static final Gson GSON = new Gson();
 
     public static void main(String[] args) {
+        SQLibConfig.setCustomConfigPath(Path.of("./lyrne/config"));
+        SQLibConfig.setCustomDefaultDirectory(Path.of("./lyrne/db"));
+
         Javalin.create(config -> {
             config.bundledPlugins.enableRouteOverview("/");
             config.staticFiles.add("web");
---
+++ backend/src/main/java/com/lyrne/backend/services/DatabaseManager.java
@@ -0,0 +1,33 @@
+package com.lyrne.backend.services;
+
+import com.lyrne.backend.User;
+import me.mrnavastar.sqlib.SQLib;
+import me.mrnavastar.sqlib.api.DataStore;
+import me.mrnavastar.sqlib.api.database.Database;
+
+import java.util.ArrayList;
+
+public class DatabaseManager {
+    
+    private static final Database DB = SQLib.getDatabase();
+    // This is an example of how to setup a data store
+    private static final DataStore someCoolStore = DB.dataStore("lyrne", "someCoolStore");
+
+    public static User getUser(String id) {
+
+        // get a user by their id, create a new user no user could be found
+
+        return null;
+    }
+
+    public static void saveUser(User user) {
+        // Save user to db
+    }
+
+    public static ArrayList<User> searchUsers(String search) {
+
+        // search for users by some query? idk lol
+
+        return new ArrayList<>();
+    }
+}
