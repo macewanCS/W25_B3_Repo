348f448656d5ef499b1bccdd1d3802ba1c3a117c
ElijahG-ME <greige@mymacewan.ca>
2025-03-08 21:24:20 -0700

    sign in with google button shows up, no function

--- frontend/components/ui/SignInButtonGoogle.tsx
+++ frontend/components/ui/SignInButtonGoogle.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Text, View, Button, useColorScheme } from 'react-native';
 import { useSession } from "@/components/Context";
+import {
+    GoogleSignin,
+    isSuccessResponse,
+    isErrorWithCode,
+    GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 
 export function GoogleSignIn() {
     const { signIn } = useSession();
@@ -8,15 +14,26 @@ export function GoogleSignIn() {
     const textColor = colorScheme === 'dark' ? 'white' : 'black';
     
     return (
-        // Placeholder for Google sign-in button
-        <View>
-            <Text style={{ color: textColor, paddingBottom: 10 }}>
-                This is a temporary button:
-            </Text>
-            <Button title="Sign In" onPress={() => {
-                 signIn("tempToken");
-                }} 
-            />
-        </View>
+        <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={async () => {
+                try {
+                    await GoogleSignin.hasPlayServices();
+                    const response = await GoogleSignin.signIn();
+                    if (isSuccessResponse(response)) {
+                        const {idToken, user} = response.data;
+                        const {name, email, photo} = user;
+                        signIn(idToken);
+
+                    }
+                }
+                catch {
+                    //lol
+                }
+
+            // initiate sign in
+            }}
+      />
     );
 }
