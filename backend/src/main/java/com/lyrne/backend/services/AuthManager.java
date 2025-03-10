f0ef7ffb8c48ff72909bf0d8d133a9bba7ef7cca
MrNavaStar <mr.navastar@gmail.com>
2025-03-09 21:04:31 -0600

    maybe fix google auth

--- backend/src/main/java/com/lyrne/backend/services/AuthManager.java
+++ backend/src/main/java/com/lyrne/backend/services/AuthManager.java
@@ -1,7 +1,6 @@
 package com.lyrne.backend.services;
 
-import com.auth0.jwk.JwkProvider;
-import com.auth0.jwk.JwkProviderBuilder;
+import com.auth0.jwk.*;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -17,6 +16,7 @@ import java.net.URI;
 import java.net.URL;
 import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -27,10 +27,7 @@ public class AuthManager {
 
     @SneakyThrows
     public static void registerProvider(String key, String url) {
-        JwkProvider jwkProvider = new JwkProviderBuilder(url)
-                .cached(10, 24, TimeUnit.HOURS)
-                .rateLimited(10, 1, TimeUnit.MINUTES)
-                .build();
+        JwkProvider jwkProvider = new GuavaCachedJwkProvider(new UrlJwkProvider(url), 10, Duration.ofSeconds(TimeUnit.HOURS.toSeconds(24)));
 
         if (key == null) {
             URL providerUrl = new URI(url).toURL();
