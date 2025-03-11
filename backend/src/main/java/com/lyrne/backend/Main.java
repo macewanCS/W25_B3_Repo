f08e56b58bf6ce3b8dc9153834bd561f1610bf5b
MrNavaStar <43613843+MrNavaStar@users.noreply.github.com>
2025-03-11 08:49:34 -0600

    correctly forward paths to cdn

--- backend/src/main/java/com/lyrne/backend/Main.java
+++ backend/src/main/java/com/lyrne/backend/Main.java
@@ -33,8 +33,8 @@ public class Main {
                 .before("/api/private/*", AuthManager::authenticate)
 
                 // Forward all cdn requests to our cdn service
-                .before("/api/private/cdn", CdnManager::forwardRequest)
-                .before("/api/cdn", CdnManager::forwardRequest)
+                .before("/api/private/cdn/*", CdnManager::forwardRequest)
+                .before("/api/cdn/*", CdnManager::forwardRequest)
 
                 // Handle fetching user data
                 .get("/api/private/user", ctx -> Optional.ofNullable(ctx.sessionAttribute("user"))
