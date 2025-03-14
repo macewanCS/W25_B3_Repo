557aaaf4dff7c016462ce3bf4a775ddac794784c
ElijahG-ME <greige@mymacewan.ca>
2025-03-14 15:02:08 -0600

    in progress

--- backend/src/main/java/com/lyrne/backend/services/Statistics.java
+++ backend/src/main/java/com/lyrne/backend/services/Statistics.java
@@ -1,4 +1,6 @@
 package com.lyrne.backend.services;
+import java.util.Optional;
+import com.lyrne.backend.TimeSlot;
 import com.lyrne.backend.services.DatabaseManager;
 import lombok.Getter;
 import me.mrnavastar.sqlib.api.DataContainer;
@@ -6,20 +8,33 @@ import me.mrnavastar.sqlib.api.types.JavaTypes;
 
 @Getter
 public class Statistics {
-    // ill work on this more tomorrow if anyone is reading this on thursday
 
-    public Integer users;
-    public Integer tutors;
-    public Integer students;
-    public Integer timeslots;
-    public Integer bookedTimeslots;
-    public Integer math;
-    public Integer science;
-    public Integer social;
-    public Integer english;
-    public Integer biology;
-    public Integer chemistry;
-    public Integer physics;
+
+    public Integer users = 0; // # of users
+    public Integer tutors = 0; // # of users that are tutors
+    public Integer students = 0; // # of users that are students
+    public Integer timeslots = 0; // you get the idea
+    public Integer bookedTimeslots = 0;
+    public Integer math = 0;
+    public Integer science = 0;
+    public Integer social = 0;
+    public Integer english = 0;
+    public Integer biology = 0;
+    public Integer chemistry = 0;
+    public Integer physics = 0;
+
+        public void generateStatistics(){
+
+
+        }
+        private void timeslotStatistics(){
+            for(DataContainer dc : timeSlotStore.getContainers()) {
+                TimeSlot ts = new TimeSlot(dc);
+                this.timeslots += 1
+                if ts.isBooked() this.bookedTimeslots += 1
+                
+                }
+        }
     
     
 
