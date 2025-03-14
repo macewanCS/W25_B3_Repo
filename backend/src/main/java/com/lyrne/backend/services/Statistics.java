b9209c20f2f21350ff597b3c82c4c01d0143f8ab
ElijahG-ME <greige@mymacewan.ca>
2025-03-14 14:49:21 -0600

    in progress

---
+++ backend/src/main/java/com/lyrne/backend/services/Statistics.java
@@ -0,0 +1,26 @@
+package com.lyrne.backend.services;
+import com.lyrne.backend.services.DatabaseManager;
+import lombok.Getter;
+import me.mrnavastar.sqlib.api.DataContainer;
+import me.mrnavastar.sqlib.api.types.JavaTypes;
+
+@Getter
+public class Statistics {
+    // ill work on this more tomorrow if anyone is reading this on thursday
+
+    public Integer users;
+    public Integer tutors;
+    public Integer students;
+    public Integer timeslots;
+    public Integer bookedTimeslots;
+    public Integer math;
+    public Integer science;
+    public Integer social;
+    public Integer english;
+    public Integer biology;
+    public Integer chemistry;
+    public Integer physics;
+    
+    
+
+}
