2d3dd4a5cc280e4073981098c8a9bf33f5c7d7b8
MrNavaStar <mr.navastar@gmail.com>
2025-03-24 20:41:54 -0600

    fix backend.tsx

--- frontend/util/Backend.tsx
+++ frontend/util/Backend.tsx
@@ -2,12 +2,12 @@ const backendUrl = "https://lyrne.mrnavastar.me"
 
 //TODO: add some form of caching to this class to reduce lookups and improve the app responsiveness
 
-export async function makeBackendRequest(session: any, method: string, endpoint: string, body: any, params: string): Promise<any> {
+export async function makeBackendRequest(session: any, method: string, endpoint: string, body: any): Promise<any> {
     if (!session.jwt) {
         throw new Error(`Invalid token: ${session.jwt}`);
     }
 
-    const response = await fetch(backendUrl + endpoint + params, {
+    const response = await fetch(backendUrl + endpoint, {
         method: method,
         headers: {
             "Authorization": `${session.jwt}`,
@@ -24,22 +24,19 @@ export async function makeBackendRequest(session: any, method: string, endpoint:
 }
 
 export async function fetchSubjects(session: any): Promise<any[]> {
-    return makeBackendRequest(session, "GET", "/api/subjects", "", "")
+    return makeBackendRequest(session, "GET", "/api/subjects", "")
 }
 
 export async function fetchUserData(session: any): Promise<any> {
-    return makeBackendRequest(session, "GET", "/api/private/user", "", "")
+    return makeBackendRequest(session, "GET", "/api/private/user", "")
 }
 
 export async function updateUserData(data: any, session: any): Promise<any> {
-    return makeBackendRequest(session, "POST", "/api/private/user", JSON.stringify(data), "")
+    return makeBackendRequest(session, "POST", "/api/private/user", JSON.stringify(data))
 }
 
 export async function fetchTutors(session: any, offset: int, subject: string): Promise<any> {
-    return makeBackendRequest(session, "GET", "/api/private/tutors", "", "?" + new URLSearchParams({
-        offset: offset,
-        subject: subject,
-    }));
+    return makeBackendRequest(session, "GET", "/api/private/tutors?offset=" + offset + "&subject=" + subject, "");
 }
 
 function parseCdnResponse(data: any) {
@@ -49,19 +46,19 @@ function parseCdnResponse(data: any) {
 export async function uploadImage(session: any, image: any): Promise<any> {
     const form = new FormData();
     form.append('image', image);
-    return parseCdnResponse(makeBackendRequest(session, "POST", "/api/private/cdn/api/cdn/upload/image", form, ""))
+    return parseCdnResponse(makeBackendRequest(session, "POST", "/api/private/cdn/api/cdn/upload/image", form))
 }
 
 export async function getImage(session: any, imageName: string): Promise<any> {
-    return parseCdnResponse(makeBackendRequest(session, "GET", "/api/private/cdn/api/cdn/download/images/" + imageName, "", ""))
+    return parseCdnResponse(makeBackendRequest(session, "GET", "/api/private/cdn/api/cdn/download/images/" + imageName, ""))
 }
 
 export async function uploadDocument(session: any, image: any): Promise<any> {
     const form = new FormData();
     form.append('doc', image);
-    return parseCdnResponse(makeBackendRequest(session, "POST", "/api/private/cdn/api/cdn/upload/doc", form, ""))
+    return parseCdnResponse(makeBackendRequest(session, "POST", "/api/private/cdn/api/cdn/upload/doc", form))
 }
 
 export async function getDocument(session: any, imageName: string): Promise<any> {
-    return parseCdnResponse(makeBackendRequest(session, "GET", "/api/private/cdn/api/cdn/download/docs/" + imageName, "", ""))
+    return parseCdnResponse(makeBackendRequest(session, "GET", "/api/private/cdn/api/cdn/download/docs/" + imageName, ""))
 }
