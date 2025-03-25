9320a53ed0290b17e968f593eca44b4b18dc374f
MrNavaStar <mr.navastar@gmail.com>
2025-03-25 02:43:01 -0600

    more cdn

--- backend/src/main/java/com/lyrne/backend/services/CdnManager.java
+++ backend/src/main/java/com/lyrne/backend/services/CdnManager.java
@@ -16,26 +16,18 @@ public class CdnManager {
 
     public static void forwardRequest(Context ctx) {
         try {
-            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(CDN_URL)).method(ctx.method().toString(), getRequestBody(ctx));
-            ctx.headerMap().forEach(requestBuilder::header);
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder(URI.create(CDN_URL));
+            ctx.headerMap().forEach((key, value) -> {
+                if (!key.equals("Authorization")) requestBuilder.header(key, value);
+            });
+            requestBuilder.method(ctx.method().name(), HttpRequest.BodyPublishers.ofByteArray(ctx.bodyAsBytes()));
 
             HttpResponse<byte[]> response = client.send(requestBuilder.build(), HttpResponse.BodyHandlers.ofByteArray());
-
             ctx.status(response.statusCode());
-            response.headers().map().forEach((key, values) -> values.forEach(value -> {
-                if (!key.equals("Authorization")) ctx.header(key, value);
-            }));
+            response.headers().map().forEach((key, values) -> values.forEach(value -> ctx.header(key, value)));
             ctx.result(response.body());
         } catch (Exception e) {
             ctx.status(500).result("CDN error: " + e.getMessage());
-        }
-    }
-
-    private static HttpRequest.BodyPublisher getRequestBody(Context ctx) throws IOException {
-        if (ctx.body().isEmpty()) return HttpRequest.BodyPublishers.noBody();
-
-        try (InputStream body = ctx.bodyInputStream()) {
-            return HttpRequest.BodyPublishers.ofInputStream(() -> body);
         }
     }
 }
